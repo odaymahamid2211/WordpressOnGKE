29d218592a7fee3bcda81f24201d7a3d9bc2f81f
odaymahamid2211 <126683590+odaymahamid2211@users.noreply.github.com>
2023-08-06 17:25:09 +0300

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,66 +1,70 @@
-/*
-# define the GCP authentication file
-variable "gcp_auth_file" {
-  type = string
-  description = "GCP authentication file"
-}
-*/
 
-# define GCP project name
+
+variable "initial_node_count" {
+  type = string
+  description = "initial_node_count"
+}
+
+variable "node_count" {
+  type = string
+  description = "node_count"
+}
+
+variable "min_node_count" {
+  type = string
+  description = "min_node_count"
+}
+
+variable "max_node_count" {
+  type = string
+  description = "max_node_count"
+}
 variable "project_id" {
   type = string
   description = "GCP project name"
 }
 
-# define GCP APP name
+
 variable "app_name" {
   type = string
   description = "Deploy App name"
 }
 
-# define GCP region
+
 variable "region" {
   type = string
   description = "GCP region"
 }
-# define GCP zone
+
 variable "zone" {
   type = string
   description = "GCP zone"
 }
 
-# define initial_node_count
-variable "initial_node_count" {
+variable "domain_name" {
   type = string
-  description = "initial_node_count"
-}
-# define node_count
-variable "node_count" {
-  type = string
-  description = "node_count"
-}
-# define min_node_count
-variable "min_node_count" {
-  type = string
-  description = "min_node_count"
-}
-# define max_node_count
-variable "max_node_count" {
-  type = string
-  description = "max_node_count"
+  description = "domain_name"
 }
 
-# define ip subnet
+variable "cluster_name" {
+  type = string
+  description = "cluster_name"
+}
+
+variable "wordpress_version" {
+  type = string
+}
+
 variable "ip_cidr_range" {
   type = string
   description = "ip_cidr_range"
 }
+
 variable "ip_cluster_range" {
   type = string
   description = "ip_cluster_range"
 }
 
-# define kubernetes deployment
 variable "replicas" {
   type = string
   description = "replicas"
@@ -79,14 +83,3 @@ variable "db_name" {
   description = "db_name"
 }
 
-variable "domain_name" {
-  type = string
-  description = "domain_name"
-}
-variable "cluster_name" {
-  type = string
-  description = "cluster_name"
-}
-variable "wordpress_version" {
-  type = string
-}
